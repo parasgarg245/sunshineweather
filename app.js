f8844daa98295db641041ba8afbeb4e1a4d1f239
parasgarg245 <parasgarg245@gmail.com>
2020-10-20 17:10:08 +0530

    this is first commit

--- app.js
+++ app.js
@@ -1,60 +1,16 @@
 const express=require("express");
-const https=require("https");
-const bodyParser=require("body-parser");
-const ejs=require("ejs"); 
-
 const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine','ejs')
+
 app.use(express.static('public'))
 
-var weatherdata=null;
 
 app.get("/",function(req,res) {
     res.sendFile(__dirname+"/index.html");
    
 });
 
-app.post("/",function(req,res){
-        var cityname=req.body.cityname;
-        const url="https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&APPID=ae4855f4718172592748594c8ebd215e&units=metric";
-        
-        https.get(url,function(response){
-
-            console.log(response.statusCode);
-
-            response.on("data",function(data){
-                 weatherdata=JSON.parse(data);
-                const temp =weatherdata.main.temp;
-                const description=weatherdata.weather[0].description;
-                const icon=weatherdata.weather[0].icon;
-                
-                const imageurl="http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
 
-
-                res.write("<p>description of temperature is "+ description +"</p>\n");
-                res.write(" <h1>the temp is " + temp + "</h1>");
-                res.write("<img src="+ imageurl+">");
-
-                res.send();
-        })
-    });
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000,function(){
+app.listen(3000 || process.env,function(){
     console.log("server is runiing on port 3000");
 });
---
+++ public/javascript/request.js
@@ -0,0 +1,14 @@
+const key = 'ae4855f4718172592748594c8ebd215e';
+
+const requestCity = async (city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
+
+    //promise data
+    const data = await response.json();
+    return data;
+
+}
