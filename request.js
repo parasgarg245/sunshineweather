2c8fef5c5ae6bc33898cd6d2ee5a120710bec32c
parasgarg245 <55296492+parasgarg245@users.noreply.github.com>
2020-10-21 23:58:15 +0530

    change in requres

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 const key = 'ae4855f4718172592748594c8ebd215e';
 
 const requestCity = async (city) => {
-    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
